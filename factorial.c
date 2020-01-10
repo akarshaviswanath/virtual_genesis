91634737a8a3f4766f74144b1f739101812812eb
akarshaviswanath <59721360+akarshaviswanath@users.noreply.github.com>
2020-01-10 14:36:03 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+  int i, n, f = 1;
+
+  printf("Enter a number to calculate its factorial\n");
+  scanf("%d", &n);
+  for (i = 1; i <= n; i++)
+    f = f * c;
+
+  printf("Factorial of %d = %d\n", n, f);
+
+  return 0;
+}

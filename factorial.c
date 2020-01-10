cb51d0801e56605a8c7d22d52e64cff44381e1a0
akarshaviswanath <59721360+akarshaviswanath@users.noreply.github.com>
2020-01-10 14:49:53 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -6,9 +6,13 @@ int main()
 
   printf("Enter a number to calculate its factorial\n");
   scanf("%d", &n);
+  if (n < 0)
+    printf("factorial for negative number can't be calculated");
+  else
+  {
   for (i = 1; i <= n; i++)
-    f = f * c;
-
+    f = f * i;
+  }
   printf("Factorial of %d = %d\n", n, f);
 
   return 0;
